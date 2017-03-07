f0cf0d69004c7d3aaebf3037ce825189a98a1fdd
Lubos Dolezel <lubos@dolezel.info>
2017-03-07 15:28:32 +0100

    Build Python 2.7 as Mach-O, except for scproxy module (darlinghq/darling#257)

--- 2.7/Python-2.7.10/Modules/getpath.c
+++ 2.7/Python-2.7.10/Modules/getpath.c
@@ -118,7 +118,7 @@
 
 #ifndef PYTHONPATH
 #define PYTHONPATH PREFIX "/lib/python" VERSION ":" \
-              EXEC_PREFIX "/lib/python" VERSION "/lib-dynload" SUFFIX
+              EXEC_PREFIX "/lib/python" VERSION "/lib-dynload"
 #endif
 
 #ifndef LANDMARK
